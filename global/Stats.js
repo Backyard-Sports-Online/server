ffe1141534adce69d3eeea3536358b2e40461bd7
shkupfer <shkupfer@ncsu.edu>
2021-12-14 19:35:25 -0500

    Handle disconnects

--- global/Stats.js
+++ global/Stats.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const StatsFormatters = {
+    // These take a statistics object from redis or the site/Mongo
+    // and return an array in the order that the game expects
     "baseball": (stats) => {
         const numericStats = Object.fromEntries(
             Object.entries(stats).map(([k, stat]) => [k, Number(stat)])
@@ -45,6 +47,8 @@ const StatsFormatters = {
 };
 
 const ResultsMappers = {
+    // These take an array that the game sends to `/game_results`
+    // and return an ongoing results object that can be stored in redis
     "baseball": (resultsFields, resultsSide) => {
         const ongoingResults = {
             winning: resultsFields[0],
@@ -72,19 +76,11 @@ const ResultsMappers = {
 };
 
 const Aggregators = {
+    // These combine a completed game's stats with that user's existing stats
+    // so that the user's stats can be updated to include that game
     "baseball": (finalResults, stats) => {
         const homeRuns = finalResults.hits - (finalResults.singles + finalResults.doubles + finalResults.triples);
-        const winSign = finalResults.winning * 2 - 1;
-        stats.wins += finalResults.winning;  // Increment user's wins
-        stats.losses += (1 - finalResults.winning);  // Increment user's losses
-        if (Math.sign(stats.streak) == winSign) {
-            // If user is continuing their streak, increment/decrement it.
-            stats.streak += winSign;
-        } else {
-            // If user is starting a new streak.
-            stats.streak = winSign;
-        }
-        // TODO (maybe): Wins in last 10 games and margin
+        // Update all of these counting stats and longest home run regardless of whether the user disconnected
         stats.games += 1;
         stats.atBats += finalResults.atBats;
         stats.hits += finalResults.hits;
@@ -96,6 +92,22 @@ const Aggregators = {
         stats.strikeouts += finalResults.strikeouts;
         stats.walks += finalResults.walks;
         stats.longestHomeRun = Math.max(stats.longestHomeRun, finalResults.longestHomeRun);
+        // If the user disconnected, increment those. If not, update wins, losses, and streak as normal
+        if (finalResults.disconnect == 1) {
+            stats.disconnects += finalResults.disconnect;
+        } else {
+            stats.wins += finalResults.winning;  // Increment user's wins
+            stats.losses += (1 - finalResults.winning);  // Increment user's losses
+            const winSign = finalResults.winning * 2 - 1;
+            if (Math.sign(stats.streak) == winSign) {
+                // If user is continuing their streak, increment/decrement it.
+                stats.streak += winSign;
+            } else {
+                // If user is starting a new streak.
+                stats.streak = winSign;
+            }
+            // TODO (maybe): Wins in last 10 games and margin (what is that exactly?)
+        }
         return stats;
     },
     // TODO: Football
